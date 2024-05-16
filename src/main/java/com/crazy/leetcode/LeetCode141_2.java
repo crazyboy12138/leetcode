7ca1fd08412dcdb957f564d0a89a7756e6a30962
lintm001 <lintm001@cmft.com>
2024-05-16 15:18:25 +0800

    leetcode 141 补充注释

--- src/main/java/com/crazy/leetcode/LeetCode141_2.java
+++ src/main/java/com/crazy/leetcode/LeetCode141_2.java
@@ -10,9 +10,17 @@ public class LeetCode141_2 {
             return false;
         }
 
+        // 快慢指针，快指针速度是慢指针的2倍
+        ListNode slow = head, fast = head;
+
+        /*
+            如果链表有环，在maxDistance最大距离内，快慢指针一定能重合。
+            假设链表节点数是n（n < 10000），
+            跑完第一圈后，快指针与慢指针差距为k，k<=n。
+            再跑第二圈，慢指针每跑1步，2个指针的距离缩小1，第二圈跑完之前一定会重合。
+         */
         int maxDistance = 10000 * 2;
 
-        ListNode slow = head, fast = head;
         for (int i = 0; i < maxDistance; i++) {
             if (slow.next == null) {
                 return false;
